d79dc7fcb5f3b73083df1f4002a12d2fb4fa05ec
Rajan Shah <rajan.shah@solutionanalysts.com>
2022-11-17 16:08:49 +0530

    [feautre] Player list & price update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PlayersService } from './shared/services/players/players.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'SPL-2022-Dec-Admin';
+  players: any;
+  constructor(private router: Router, private playerService: PlayersService) {}
+
+  async getPlayers() {
+    const response: any = await this.playerService.getPlayers();
+    this.players = response.data;
+  }
+
+  redirect(index: any) {
+    this.router.navigateByUrl('players');
+  }
+
+  async bidDone(player: any,index:any) {
+    player.currentPrice = this.getNewBidPrice(player);
+    this.players[index] = player;
+    const response: any = await this.playerService.updatePlayerBid(player.id, {
+      currentPrice: player.currentPrice,
+    });
+  }
+
+  getNewBidPrice(player: any) {
+    if ((+player.currentPrice - +player.basePrice) > 100000) {
+      return +player.currentPrice + 50000
+    } else if(player.currentPrice) {
+      return +player.currentPrice + 25000
+    }else{
+      return +player.basePrice + 25000
+
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
